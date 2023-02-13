ba44c71c4f02b3e57fb7328a81dcf9c0e736017a
MaxSlipchuk <nice.slipchukmax@gmail.com>
2023-02-13 16:22:35 +0200

    write "win"

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import turtle
-import create_screen as c_screen
-import draw_table as d_table
-import cell_click_detection as c_click
+import modules.create_screen as c_screen
+import modules.draw_table as d_table
+import modules.cell_click_detection as c_click
 
 
 d_table.draw_table(-100,100)
